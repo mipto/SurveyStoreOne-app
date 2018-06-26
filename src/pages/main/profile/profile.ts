79601d4fcbeefe26b92424b6081a7e76fbfa6d46
Unknown <edgard1394@hotmail.com>
2018-06-26 16:55:22 -0400

    Auto stash before merge of "profile_branch" and "origin/develop"

--- src/pages/main/profile/profile.ts
+++ src/pages/main/profile/profile.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController, ToastController } from 'ionic-angular';
 import { AuthData } from '../../../providers/auth-data';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Globals } from '../../../services/globals.service';
-import { UserService } from '../../../services/user.service';
+import { Globals } from '../../../providers/globals';
+import { last } from 'rxjs/operator/last';
 
 @IonicPage()
 @Component({
@@ -13,6 +13,10 @@ import { UserService } from '../../../services/user.service';
 export class ProfilePage {
 
   public profileForm: any;
+  user = {} as User;
+  firstNameEdit: string;
+  lastNameEdit: string;
+  phoneEdit: number;
   onEdit: boolean;
   validEdit: boolean;
   constructor(public navCtrl: NavController,
@@ -35,6 +39,17 @@ export class ProfilePage {
 
   }
 
+  ionViewWillEnter() {
+    let ion = this;
+    ion.authData.getUserProfile().then(userProfileData => {
+      ion.user = userProfileData;
+      ion.firstNameEdit = userProfileData.first_name;
+      ion.lastNameEdit = userProfileData.last_name;
+      ion.phoneEdit = userProfileData.phone;
+      ion.user.email = ion.authData.getAuthUser().email;
+    });
+  }
+
   edit() {
     let ion = this;
     if (ion.onEdit == false) {
@@ -44,16 +59,25 @@ export class ProfilePage {
     }
   }
 
-  save(user) {
+  save(first_name, last_name, phone) {
     let ion = this;
+
     if (!ion.profileForm.valid) {
       ion.presentToast("top", "no sirvio");
     }
     else {
+      let user = {
+        first_name: first_name,
+        last_name: last_name,
+        phone: phone,
+      }
       ion.authData.updateProfile(user)
         .then(function () {
           console.log('Saved successfully');
           ion.validEdit = false;
+          ion.user.first_name = first_name;
+          ion.user.last_name = last_name;
+          ion.user.phone = phone;
           ion.edit();
         }).catch(error => {
           console.log("Error updating data:", error);
